7abace37cc65cca9018a5e4488dd8f6c35059e6e
AbdEl-WahabGhareeb <abdosileem48@gmail.com>
2025-09-22 19:53:19 +0300

    Deploying

--- src/app/(pages)/category/page.tsx
+++ src/app/(pages)/category/page.tsx
@@ -9,9 +9,7 @@ export default async function Category() {
             <div className="container  mx-auto">
                 <div className="grid grid-cols-1 md:grid-cols-3 pt-24 gap-6">
                     <CategoryDisplay category={categories?.data} />
-                    
                 </div>
-       
             </div>
         </>
     );
