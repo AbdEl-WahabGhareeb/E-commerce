326c6bd210a230f9a7f8ecfd2bdb11be6672a45c
AbdEl-WahabGhareeb <abdosileem48@gmail.com>
2025-09-23 09:43:25 +0300

    Deploying

--- src/lib/token.utils.ts
+++ src/lib/token.utils.ts
@@ -1,43 +1,43 @@
-import { decode } from "next-auth/jwt";
-import { cookies } from "next/headers";
-
-export async function getUserToken() {
-    const cookieStore = await cookies();
-
-    const encodedToken =
-        cookieStore.get("next-auth.session-token")?.value ||
-        cookieStore.get("__Secure-next-auth.session-token")?.value;
-
-    if (!encodedToken) {
-        return undefined;
-    }
-
-    const decodeToken = await decode({
-        token: encodedToken,
-        secret: process.env.NEXTAUTH_SECRET!,
-    });
-
-    const token = decodeToken?.token;
-
-    return token;
-}
-
-
 // import { decode } from "next-auth/jwt";
 // import { cookies } from "next/headers";
 
 // export async function getUserToken() {
-//     const encodedToken = (await cookies()).get(
-//         "next-auth.session-token"
-//     )?.value;
-//     const decryptedToken = await decode({
-//         token: encodedToken,
-//         secret: process.env.AUTH_SECRET!,
-//     });
-//     const token = decryptedToken?.token;
+//     const cookieStore = await cookies();
 
-//     console.log(token);
-    
+//     const encodedToken =
+//         cookieStore.get("next-auth.session-token")?.value ||
+//         cookieStore.get("__Secure-next-auth.session-token")?.value;
+
+//     if (!encodedToken) {
+//         return undefined;
+//     }
+
+//     const decodeToken = await decode({
+//         token: encodedToken,
+//         secret: process.env.NEXTAUTH_SECRET!,
+//     });
+
+//     const token = decodeToken?.token;
 
 //     return token;
 // }
+
+
+import { decode } from "next-auth/jwt";
+import { cookies } from "next/headers";
+
+export async function getUserToken() {
+    const encodedToken = (await cookies()).get(
+        "next-auth.session-token"
+    )?.value;
+    const decryptedToken = await decode({
+        token: encodedToken,
+        secret: process.env.AUTH_SECRET!,
+    });
+    const token = decryptedToken?.token;
+
+    console.log(token);
+    
+
+    return token;
+}
